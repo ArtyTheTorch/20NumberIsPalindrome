cf520f971a09054fdb2aa86adac646023053239e
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 12:07:55 -0700

    Checks a integer to see if it is palindrome
    Similar to the String palindrome however to reverse the number we use
    arithmetic.
    Console-Implementation

---
+++ 20NumberIsPalindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20NumberIsPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(reverse(num));
+            Console.WriteLine(reverse(num)==num);
+            Console.ReadLine();
+        }
+
+        private static int reverse(int num)
+        {
+            int results = 0;
+            int temp = num;
+
+            do {
+                results = results*10 + temp % 10;
+                temp = temp / 10;
+            }
+            while (temp > 0);
+            return results;
+        }
+    }
+}
